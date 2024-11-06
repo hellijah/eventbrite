5799d7665efdb16e19d999c349d83067c58cbc4a
hellijah <thierry.corlieto@gmail.com>
2024-11-06 18:36:43 +0100

    modif db length validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,8 @@ class Event < ApplicationRecord
 
   validates :start_date, presence: true
   validates :duration, presence: true
-  validates :title, presence: true, length: { in: 5..140 }
-  validates :description, presence: true, length: { in: 20..1000 }
+  validates :title, presence: true, length: { in: 3..140 }
+  validates :description, presence: true, length: { in: 4..1000 }
   validates :price, presence: true, inclusion: { in: 1..1000 }
   validates :location, presence: true
 end
