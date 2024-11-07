8ed37c493157319407c17f4289f1cec4e06a597e
hellijah <thierry.corlieto@gmail.com>
2024-11-07 15:58:37 +0100

    modif stripe controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,26 +1,41 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
-  def create_checkout_session
-    @event = Event.find(params[:event_id])
-
-    session = Stripe::Checkout::Session.create(
+  def create
+    @total = params[:total].to_d
+    @event_id = params[:event_id]
+    @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
-      line_items: [{
-        price_data: {
-          currency: 'eur',
-          product_data: {
-            name: @event.title,
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            unit_amount: (@total*100).to_i,
+            product_data: {
+              name: 'Rails Stripe Checkout',
+            },
           },
-          unit_amount: @event.price * 100, # Montant en centimes
+          quantity: 1
         },
-        quantity: 1,
-      }],
+      ],
       mode: 'payment',
-      success_url: event_url(@event) + '?payment=success',
-      cancel_url: event_url(@event) + '?payment=cancel',
+      metadata: { event_id: @event_id },
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
     )
-
-    redirect_to session.url, allow_other_host: true
+    redirect_to @session.url, allow_other_host: true
   end
+
+def success
+  @session = Stripe::Checkout::Session.retrieve(params(:session_id))
+  @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  @event_id = @session.metadata.event_id
+  
+  stripe_customer_id = "stripe_#{SecureRandom.hex(10)}" #Unique User ID
+  Attendance.Create(user: current_user, event_id: @event_id, stripe_customer_id: stripe_customer_id)
+
+end
+
+def cancel_url
+
 end
